3bdddabd7bcf99b07a574a9cf9fd0e932fcbd5e8
nicolasph <nicolasph@localhost>
2009-12-02 10:32:45 +0100

    Imported default branch to fw4spl 0.5 branch

---
+++ SrcLib/core/fwComEd/src/fwComEd/PointMsg.cpp
@@ -0,0 +1,31 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as 
+ * published by the Free Software Foundation.  
+ * ****** END LICENSE BLOCK ****** */
+
+#include "fwComEd/PointMsg.hpp"
+
+
+namespace fwComEd
+{
+
+//-----------------------------------------------------------------------------
+
+std::string PointMsg::POINT_IS_MODIFIED       = "POINT_IS_MODIFIED";
+std::string PointMsg::START_POINT_INTERACTION = "START_POINT_INTERACTION";
+
+//-----------------------------------------------------------------------------
+
+PointMsg::PointMsg() throw()
+{}
+
+//-----------------------------------------------------------------------------
+
+PointMsg::~PointMsg() throw()
+{}
+
+//-----------------------------------------------------------------------------
+
+} // namespace fwComEd
+
