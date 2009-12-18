eba86209997191d5247b3a30d1a3194bb5c6c1b9
jmoreau <jmoreau@IRCAD.FR>
2009-12-18 18:10:23 +0100

    Add new wxs file to package

---
+++ SrcLib/core/fwData/src/fwData/String.cpp
@@ -0,0 +1,30 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as 
+ * published by the Free Software Foundation.  
+ * ****** END LICENSE BLOCK ****** */
+
+#include <fwTools/ClassRegistrar.hpp>
+
+#include "fwData/GenericField.hpp"
+#include "fwData/String.hpp"
+
+REGISTER_BINDING_BYCLASSNAME( ::fwTools::Object, ::fwData::String, ::fwData::String );
+
+namespace fwData
+{
+
+//------------------------------------------------------------------------------
+
+String::String( const std::string value ) throw()
+:   GenericField< std::string >( value )
+{}
+
+//------------------------------------------------------------------------------
+
+String::~String() throw()
+{}
+
+//------------------------------------------------------------------------------
+
+} // namespace fwData
