ccd37a37069560756e8b2aea934a8a9d68da5367
arnaud charnoz <arnaud.charnoz@gmail.com>
2009-12-23 12:05:42 +0100

    Created branch fw4spl_0.6

---
+++ SrcLib/core/fwComEd/src/fwComEd/ReconstructionMsg.cpp
@@ -0,0 +1,29 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as 
+ * published by the Free Software Foundation.  
+ * ****** END LICENSE BLOCK ****** */
+
+#include "fwComEd/ReconstructionMsg.hpp"
+
+
+namespace fwComEd
+{
+
+//-----------------------------------------------------------------------------
+std::string ReconstructionMsg::MESH = "MESH";
+std::string ReconstructionMsg::VISIBILITY = "VISIBILITY";
+//-----------------------------------------------------------------------------
+
+ReconstructionMsg::ReconstructionMsg() throw()
+{}
+
+//-----------------------------------------------------------------------------
+
+ReconstructionMsg::~ReconstructionMsg() throw()
+{}
+
+//-----------------------------------------------------------------------------
+
+} // namespace fwComEd
+
