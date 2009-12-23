def6505bb08968ac18e727452fbd468718ad4522
emilie.har <emilie.har@localhost>
2009-12-23 17:36:50 +0100

    * fix plane comparison : compare normal and distance  * update message event on PlaneListMsg and ResectionDNMsg

---
+++ SrcLib/core/fwComEd/src/fwComEd/ResectionMsg.cpp
@@ -0,0 +1,34 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * FW4SPL - Copyright (C) IRCAD, 2009.
+ * Distributed under the terms of the GNU Lesser General Public License (LGPL) as 
+ * published by the Free Software Foundation.  
+ * ****** END LICENSE BLOCK ****** */
+
+#include "fwComEd/ResectionMsg.hpp"
+
+
+namespace fwComEd
+{
+
+//-----------------------------------------------------------------------------
+
+std::string ResectionMsg::ADD_RECONSTRUCTION = "ADD_RECONSTRUCTION";
+std::string ResectionMsg::VISIBILITY = "VISIBILITY";
+std::string ResectionMsg::NEW_RECONSTRUCTION_SELECTED = "NEW_RECONSTRUCTION_SELECTED";
+std::string ResectionMsg::MODIFIED = "MODIFIED";
+
+
+//-----------------------------------------------------------------------------
+
+ResectionMsg::ResectionMsg() throw()
+{}
+
+//-----------------------------------------------------------------------------
+
+ResectionMsg::~ResectionMsg() throw()
+{}
+
+//-----------------------------------------------------------------------------
+
+} // namespace fwComEd
+
